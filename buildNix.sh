cc4a2f24ae517307433cdf28142c7269301e943a
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-02-16 08:23:54 -0500

    build on GHA (#27)
    * add arm build
    
    * slashes
    
    * update CVODES
    
    * add github packages repo
    
    * update
    
    * update source
    
    * update sln name
    
    * update CVODES
    
    * fail fast off
    
    * build mac for arm arch
    
    * run tests
    
    * vstestrunner action
    
    * specify assembly
    
    * specify assembly
    
    * specify build folder
    
    * work around
    
    * quotes
    
    * install nunit and run tests
    
    * package only the csproj
    
    * update comment

--- buildNix.sh
+++ buildNix.sh
@@ -1,17 +1,37 @@
 #! /bin/sh
 
 #call: buildLinux.sh distributionName version
-# e.g. buildLinux.sh ${1} 4.0.0.49
+# e.g. buildLinux.sh MacOS 4.0.0.49
+
+if [ `uname -m` = 'x86_64' ]; 
+then
+  ARCH=x64
+else
+  ARCH=Arm64
+fi
+
+
 
 nuget sources add -name sundials_cvodes -source https://ci.appveyor.com/nuget/cvodes
 nuget install packages.config -OutputDirectory packages -ExcludeVersion
 
 git submodule update --init --recursive
 
-cmake -BBuild/Release/x64/ -Hsrc/OSPSuite.SimModelSolver_CVODES/ -DCMAKE_BUILD_TYPE=Release -DlibCVODES=packages/CVODES.${1}/CVODES/bin/native/x64/Release/libsundials_cvodes.a
-make -C Build/Release/x64/
+if [ `uname` = 'Darwin' ]; 
+then
+  cmake -BBuild/Release/$ARCH/ -Hsrc/OSPSuite.SimModelSolver_CVODES/ -DCMAKE_BUILD_TYPE=Release -DlibCVODES=packages/CVODES.${1}$ARCH/CVODES/bin/native/$ARCH/Release/libsundials_cvodes.a
+  make -C Build/Release/$ARCH/
 
-cmake -BBuild/Debug/x64/ -Hsrc/OSPSuite.SimModelSolver_CVODES/ -DCMAKE_BUILD_TYPE=Debug -DlibCVODES=packages/CVODES.${1}/CVODES/bin/native/x64/Debug/libsundials_cvodes.a
-make -C Build/Debug/x64/
+  cmake -BBuild/Debug/$ARCH/ -Hsrc/OSPSuite.SimModelSolver_CVODES/ -DCMAKE_BUILD_TYPE=Debug -DlibCVODES=packages/CVODES.${1}$ARCH/CVODES/bin/native/$ARCH/Debug/libsundials_cvodes.a
+  make -C Build/Debug/$ARCH/
+else
+  cmake -BBuild/Release/$ARCH/ -Hsrc/OSPSuite.SimModelSolver_CVODES/ -DCMAKE_BUILD_TYPE=Release -DlibCVODES=packages/CVODES.${1}/CVODES/bin/native/$ARCH/Release/libsundials_cvodes.a
+  make -C Build/Release/$ARCH/
+
+  cmake -BBuild/Debug/$ARCH/ -Hsrc/OSPSuite.SimModelSolver_CVODES/ -DCMAKE_BUILD_TYPE=Debug -DlibCVODES=packages/CVODES.${1}/CVODES/bin/native/$ARCH/Debug/libsundials_cvodes.a
+  make -C Build/Debug/$ARCH/
+fi
+
+
 
 dotnet pack src/OSPSuite.SimModelSolver_CVODES/pack.csproj -p:PackageVersion=$2 -o ./
